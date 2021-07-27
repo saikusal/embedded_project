93428b2bfd6207f92c2e6f88e066eaa5799fe4f2
saikusal <66658250+saikusal@users.noreply.github.com>
2021-07-27 22:13:06 +0530

    Delete documentation directory

--- documentation/html/files_dup.js
+++
@@ -1,7 +0,0 @@
-var files_dup =
-[
-    [ "Activity1.h", "_activity1_8h_source.html", null ],
-    [ "Activity2.h", "_activity2_8h_source.html", null ],
-    [ "Activity3.h", "_activity3_8h_source.html", null ],
-    [ "Activity4.h", "_activity4_8h_source.html", null ]
-];
